97ac19ccbcbd38e8bc69b55b23b8683ba2d303a7
General Zelgius <mattlow123@gmail.com>
2022-03-27 12:13:36 -0400

    Added generateReadme.js, completed index.js and wrote license logic for generateMarkdown.js

--- index.js
+++ index.js
@@ -1,164 +1,155 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const { generateMarkdown } = require('./utils/generateMarkdown');
+const { writeFile } = require('./utils/generateReadMe');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'username',
-        message: 'What is your GitHub username? (No @ is necessary) (Required)',
-        validate: usernameInput => {
-            if (usernameInput) {
-                return true;
-            } else {
-                return false;
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username? (No @ is necessary) (Required)',
+            validate: usernameInput => {
+                if (usernameInput) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'repo',
-        message: 'What is the name of your GitHub repo? (Required)',
-        validate: repoInput => {
-            if (repoInput) {
-                return true;
-            } else {
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'repo',
+            message: 'What is the name of your GitHub repo? (Required)',
+            validate: repoInput => {
+                if (repoInput) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project? (Required)',
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('Please enter the name of your project!');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of your project!');
+                    return false;
+                }
+            }    
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of your project. Consider the following. What was your motivation? Why did you build this project? What problem does it solve? What did you learn?',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a description!');
+                    return false;
+                }
             }
-        }    
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please provide a description of your project. Consider the following. What was your motivation? Why did you build this project? What problem does it solve? What did you learn?',
-        validate: descriptionInput => {
-            if (descriptionInput) {
-                return true;
-            } else {
-                console.log('Please provide a description!');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'technologies',
+            message: 'What technologies did you use to build this project? (JavaScript, HTML, CSS, JQuery, etc.)',
+            validate: technologiesInput => {
+                if (technologiesInput) {
+                    return true;
+                } else {
+                    console.log('Please enter which technologies you used!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'technologies',
-        message: 'What technologies did you use to build this project? (JavaScript, HTML, CSS, JQuery, etc.)',
-        validate: technologiesInput => {
-            if (technologiesInput) {
-                return true;
-            } else {
-                console.log('Please enter which technologies you used!');
-                return false;
+        },
+        {
+            type: 'confirm',
+            name: 'confirmInstall',
+            message: 'Are there any steps necessary to install your project?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the steps necessary to install this project?',
+            when: ({ confirmInstall }) => {
+                if (confirmInstall) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'confirm',
-        name: 'confirmInstall',
-        message: 'Are there any steps necessary to install your project?',
-        default: true
-    },
-    {
-        type: 'input',
-        name: 'install',
-        message: 'What are the steps necessary to install this project?',
-        when: ({ confirmInstall }) => {
-            if (confirmInstall) {
-                return true;
-            } else {
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide instructions and examples of how to properly use this project.',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please tell us how to use your project!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please provide instructions and examples of how to properly use this project.',
-        validate: usageInput => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log('Please tell us how to use your project!');
-                return false;
+        },
+        {
+            type: 'confirm',
+            name: 'confirmCollab',
+            message: 'Did you collaborate with other people on this project?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'collaboration',
+            message: 'Please enter the names of your collaborators and provide links to their github profiles.',
+            when: ({ confirmCollab }) => {
+                if (confirmCollab) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'confirm',
-        name: 'confirmCollab',
-        message: 'Did you collaborate with other people on this project?',
-        default: false
-    },
-    {
-        type: 'input',
-        name: 'collaboration',
-        message: 'Please enter the names of your collaborators and provide links to their github profiles.',
-        when: ({ confirmCollab }) => {
-            if (confirmCollab) {
-                return true;
-            } else {
-                return false;
+        },
+        {
+            type: 'confirm',
+            name: 'confirmTest',
+            message: 'Are there any tests that have been written for your project?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Please provide any tests for your project and instructions on how to run them.',
+            when: ({ confirmTest }) => {
+                if (confirmTest) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
+        },
+        {
+            type: 'checkbox',
+            name: 'licenses',
+            message: 'Choose a license for this project',
+            choices: ['Apache License 2.0', 'BSD 3-Clause License', 'BSD 2-Clause License', 'MIT License', 'Mozilla Public License 2.0', 'Common Development and Distribution License', 'Eclipse Public License 2.0']
         }
-    },
-    {
-        type: 'confirm',
-        name: 'confirmTest',
-        message: 'Are there any tests that have been written for your project?',
-        default: false
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Please provide any tests for your project and instructions on how to run them.',
-        when: ({ confirmTest }) => {
-            if (confirmTest) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    },
-    {
-        type: 'checkbox',
-        name: 'licenses',
-        message: 'Choose a license for this project',
-        choices: ['Apache License 2.0', 'BSD 3-Clause License', 'BSD 2-Clause License', 'GNU GPL', 'GNU LGPL', 'MIT License', 'Mozilla Public License 2.0', 'Common Development and Distribution License', 'Eclipse Public License 2.0']
-    }
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('Congratulations! Your README file has been generated!');
+    ])
+    .then((responses) => {
+        writeFile(generateMarkdown(responses));
+    })
+    .catch(err => {
+        console.log(err);
     });
 };
 
-// TODO: Create a function to initialize app
-function init() {
-    const responses = await inquirer.prompt(questions);
-    console.log('User Responses: ', responses);
-
-}
-
 // Function call to initialize app
-init();
+promptUser();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,88 @@
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'Apache License 2.0') {
+    const licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    return licenseBadge;
+  
+  } else if (license === 'BSD 3-Clause License') {
+    const licenseBadge = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+    return licenseBadge;
+  
+  } else if (license === 'BSD 2-Clause License') {
+    const licenseBadge = '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)'
+    return licenseBadge;
+
+  } else if (license === 'MIT License') {
+    const licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    return licenseBadge;
+  
+  } else if (license === 'Mozilla Public License 2.0') {
+    const licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+    return licenseBadge;
+
+  } else if (license === 'Eclipse Public License 2.0') {
+    const licenseBadge = '[![License](https://img.shields.io/badge/License-EPL_2.0-red.svg)](https://opensource.org/licenses/EPL-2.0)'
+    return licenseBadge;;
+  
+  } else {
+    return '';
+  } 
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'Apache License 2.0') {
+    const licenseLink = 'https://opensource.org/licenses/Apache-2.0'
+    return licenseLink;
+  
+  } else if (license === 'BSD 3-Clause License') {
+    const licenseLink = 'https://opensource.org/licenses/BSD-3-Clause'
+    return licenseLink;
+  
+  } else if (license === 'BSD 2-Clause License') {
+    const licenseLink = 'https://opensource.org/licenses/BSD-2-Clause'
+    return licenseLink;
+
+  } else if (license === 'MIT License') {
+    const licenseLink = 'https://opensource.org/licenses/MIT'
+    return licenseLink;
+  
+  } else if (license === 'Mozilla Public License 2.0') {
+    const licenseLink = 'https://opensource.org/licenses/MPL-2.0'
+    return licenseLink;
+  
+  } else if (license === 'Common Development and Distribution License') {
+    const licenseLink = 'https://opensource.org/licenses/CDDL-1.0'
+    return licenseLink;
+
+  } else if (license === 'Eclipse Public License 2.0') {
+    const licenseLink = 'https://opensource.org/licenses/EPL-2.0'
+    return licenseLink;
+  
+  } else {
+    return '';
+  } 
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+  return(`${renderLicenseBadge(license)}`);
+  } else {
+    return '';
+  }
+};
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(responses) {
+  
 
-`;
+
 }
 
 module.exports = generateMarkdown;
